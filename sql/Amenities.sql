5399efa2c102b3ece42abcec9ac28b21eaf6fbec
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-04-03 14:58:45 +0800

    Update Amenities.sql

--- sql/Amenities.sql
+++ sql/Amenities.sql
@@ -10,13 +10,13 @@ create table Amenities (
 	washing_machine VARCHAR(3) NOT NULL CHECK (washing_machine IN ('Yes', 'No')),
 	FOREIGN KEY ID REFERENCES Catalog(ID_place)
 );
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0001', 'Yes', 'No', 'No', 'Yes', 'No', 'No', 'No', 'Yes');
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0002', 'Yes', 'Yes', 'No', 'Yes', 'Yes', 'No', 'Yes', 'No');
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0003', 'No', 'Yes', 'Yes', 'No', 'No', 'No', 'Yes', 'No');
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0004', 'No', 'No', 'No', 'No', 'No', 'No', 'Yes', 'Yes');
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0005', 'Yes', 'Yes', 'No', 'No', 'No', 'No', 'Yes', 'No');
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0006', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'No');
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0007', 'No', 'No', 'Yes', 'Yes', 'No', 'Yes', 'No', 'No');
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0008', 'Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'No', 'No');
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0009', 'No', 'No', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes');
-insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0010', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'Yes', 'No', 'No');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0001', 'Yes', 'No', 'No', 'Yes', 'No', 'No', 'No', 'Yes');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0002', 'Yes', 'Yes', 'No', 'Yes', 'Yes', 'No', 'Yes', 'No');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0003', 'No', 'Yes', 'Yes', 'No', 'No', 'No', 'Yes', 'No');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0004', 'No', 'No', 'No', 'No', 'No', 'No', 'Yes', 'Yes');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0005', 'Yes', 'Yes', 'No', 'No', 'No', 'No', 'Yes', 'No');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0006', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'No');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0007', 'No', 'No', 'Yes', 'Yes', 'No', 'Yes', 'No', 'No');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0008', 'Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'No', 'No');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0009', 'No', 'No', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes');
+insert into Amenities (ID, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('0010', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'Yes', 'No', 'No');
