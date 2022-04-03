8dc06c835dac9a793f917d0469bdd26a0a9be840
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-04-03 14:43:56 +0800

    Add files via upload

---
+++ sql/Reservation.sql
@@ -0,0 +1,19 @@
+create table Reservation (
+	guest_ID INT,
+	number_of_guests INT,
+	checkin_date DATE,
+	checkout_date DATE
+);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (1, 1, 'error: Please use field(''ID'') to access ID because it starts with an upper case letter.', '30/09/2022');
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (67, 1, 'error: Please use field(''ID'') to access ID because it starts with an upper case letter.', '21/01/2023');
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (35, 5, 'error: Please use field(''ID'') to access ID because it starts with an upper case letter.', '08/05/2022');
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
+insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (null, null, null, null);
