8888d9000f7ce6e716fddaa8cd9794083a30efe3
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-04-03 14:12:41 +0800

    Add files via upload

---
+++ sql/Amenities.sql
@@ -0,0 +1,30 @@
+create table Amenities (
+	kitchen VARCHAR(3),
+	parking VARCHAR(3),
+	wifi VARCHAR(3),
+	smoking_allowed VARCHAR(3),
+	pets_allowed VARCHAR(3),
+	air_conditioning VARCHAR(3),
+	TV VARCHAR(3),
+	washing_machine VARCHAR(3)
+);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'No', 'No', 'Yes', 'No', 'No', 'No', 'Yes');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'Yes', 'No', 'Yes', 'Yes', 'No', 'Yes', 'No');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'Yes', 'Yes', 'No', 'No', 'No', 'Yes', 'No');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'No', 'No', 'No', 'No', 'No', 'Yes', 'Yes');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'Yes', 'No', 'No', 'No', 'No', 'Yes', 'No');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'Yes', 'No', 'Yes', 'No', 'Yes', 'No', 'No');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'No', 'Yes', 'Yes', 'No', 'Yes', 'No', 'No');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'No', 'Yes', 'No', 'Yes', 'No', 'No', 'No');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'No', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values ('error: Please use field(''ID'') to access ID because it starts with an upper case letter.', 'Yes', 'Yes', 'No', 'Yes', 'Yes', 'No', 'No');
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
+insert into Amenities (kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) values (null, null, null, null, null, null, null, null);
