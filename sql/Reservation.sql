b5df895f118902c146f40a151c8f8b23d6df8b1e
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-04-03 16:37:34 +0800

    Update Reservation.sql

--- sql/Reservation.sql
+++ sql/Reservation.sql
@@ -2,9 +2,9 @@ create table Reservation (
 	ID_place VARCHAR(50) REFERENCES Catalog(ID_place),
 	ID_guest INT REFERENCES Account(ID),
 	number_of_guests INT NOT NULL,
-	checkin_date DATE NOT NULL CHECK (checkin_date > '03/04/2022'),
+	checkin_date DATE NOT NULL CHECK (checkin_date > '2022-04-03'),
 	checkout_date DATE NOT NULL CHECK (checkout_date > checkin_date)
 );
-insert into Reservation (ID_place, ID_guest, number_of_guests, checkin_date, checkout_date) values ('0001', 73, 3, '13/03/2023', '19/03/2023');
-insert into Reservation (ID_place, ID_guest, number_of_guests, checkin_date, checkout_date) values ('0004', 80, 2, '01/11/2022', '11/11/2022');
-insert into Reservation (ID_place, ID_guest, number_of_guests, checkin_date, checkout_date) values ('0007', 2, 5, '04/05/2022', '03/07/2022');
+insert into Reservation (ID_place, ID_guest, number_of_guests, checkin_date, checkout_date) values ('0001', 73, 3, '2023-03-13', '2023-03-19');
+insert into Reservation (ID_place, ID_guest, number_of_guests, checkin_date, checkout_date) values ('0004', 80, 2, '2022-11-01', '2022-11-11');
+insert into Reservation (ID_place, ID_guest, number_of_guests, checkin_date, checkout_date) values ('0007', 2, 5, '2022-05-04', '2022-07-03');
