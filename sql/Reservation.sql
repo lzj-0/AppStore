b133b028491030bfa0de44b4549d6addee00004d
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-04-03 14:53:40 +0800

    Update Reservation.sql

--- sql/Reservation.sql
+++ sql/Reservation.sql
@@ -1,9 +1,10 @@
 create table Reservation (
-	guest_ID INT,
-	number_of_guests INT,
-	checkin_date DATE,
-	checkout_date DATE
+	ID_place VARCHAR(50) REFERENCES catalog(ID_place),
+	ID_guest INT REFERENCES Account(ID),
+	number_of_guests INT NOT NULL,
+	checkin_date DATE NOT NULL CHECK (checkin_date > '03/04/2022'),
+	checkout_date DATE NOT NULL CHECK (checkout_date > checkin_date)
 );
-insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (73, 3, 'error: Please use field(''ID'') to access ID because it starts with an upper case letter.', '19/03/2023');
-insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (80, 2, 'error: Please use field(''ID'') to access ID because it starts with an upper case letter.', '11/11/2022');
-insert into Reservation (guest_ID, number_of_guests, checkin_date, checkout_date) values (2, 5, 'error: Please use field(''ID'') to access ID because it starts with an upper case letter.', '03/07/2022');
+insert into Reservation (ID_place, ID_guest, number_of_guests, checkin_date, checkout_date) values ('0001', 73, 3, '13/03/2023', '19/03/2023');
+insert into Reservation (ID_place, ID_guest, number_of_guests, checkin_date, checkout_date) values ('0004', 80, 2, '01/11/2022', '11/11/2022');
+insert into Reservation (ID_place, ID_guest, number_of_guests, checkin_date, checkout_date) values ('0007', 2, 5, '04/05/2022', '03/07/2022');
