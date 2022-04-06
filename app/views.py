9ea9711f9d44c445fa564975f40c35407ccbc891
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-04-06 23:39:36 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ def index(request):
 
     ## Use raw query to get all objects
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM Catalog ORDER BY ID_account")
+        cursor.execute("SELECT ID_place, ID_account, title, country, city, rating, price_per_night FROM Catalog ORDER BY ID_account")
         listings = cursor.fetchall()
 
     result_dict = {'records': listings}
