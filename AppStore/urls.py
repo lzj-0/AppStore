95ebad77426217dae0221f201f1b03a82b3a1cf4
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-04-08 00:45:07 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('add', app.views.add, name='add'),
     path('view/<str:id>', app.views.view, name='view'),
     path('edit/<str:id>', app.views.edit, name='edit'),
-    path('booking/', app.views.booking, name = 'booking')
+    path('booking/', app.views.view, name = 'booking')
 ]
