dedb326e1833811b5f5bb8f25ca62ad86a5b3e34
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-04-07 23:02:56 +0800

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
-from myapp.models import Author
 
-class AuthorAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Author, AuthorAdmin)
